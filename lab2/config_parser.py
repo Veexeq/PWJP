4dbbb2b23abb29b6ab6ae590f0d580306f9014c5
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-03-27 09:24:12 +0100

    Solved exercise 1.

--- lab2/config_parser.py
+++ lab2/config_parser.py
@@ -1,11 +1,26 @@
 import argparse
 from pathlib import Path
+import yaml
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=Path)
+parser = argparse.ArgumentParser(description='YAML config parser')
+parser.add_argument(
+    '--config', 
+    type=Path,
+    required=True,
+    help='Path to the YAML config file'
+)
+
 args = parser.parse_args()
 config_path = args.config
 if not config_path:
     raise Exception('No config file specified.')
 
-print(config_path)
+try:
+    with open(config_path, 'r', encoding='utf-8') as file:
+        data = yaml.safe_load(file)
+except FileNotFoundError:
+    raise Exception(f'File {config_path} not found')
+except yaml.YAMLError as err:
+    raise Exception(f'An error occured in the YAML file structure: {err}')
+
+print(data)
