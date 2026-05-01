a6580c1600e5f47730729e13790f86b6224bab1c
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-05-01 13:18:24 +0200

    Small fixes to the basic version of the exercise.

--- lab4/main.py
+++ lab4/main.py
@@ -5,21 +5,29 @@ class Student:
         self._indeks = indeks
     
     def przedstaw_sie(self) -> None:
-       print(f'Jestem {self._imie} {self._nazwisko}' 
+       print(f'Jestem {self._imie} {self._nazwisko} ' 
              f'mój numer indeksu to {self._indeks}.')
     
     def __str__(self) -> str:
         return f'Student {self._imie} {self._nazwisko}, nr indeksu {self._indeks}'
     
+def print_header(text: str, length: int = 60) -> None:
+    header = f' {text} '.center(length, '=')
+    print(header)
+    
 def test_basic() -> None:
+    print_header(text='POZIOM PODSTAWOWY')
+    
     students = [
         Student(imie='Jan', nazwisko='Nowak', indeks='389211'),
         Student(imie='Paweł', nazwisko='Kowalski', indeks='401498'),
     ]
-        
-    for student in students:
-        print(f'przedstaw_sie(): {student.przedstaw_sie()}')
-        print(f'__str__(): {student}')
+    
+    for idx, student in enumerate(students, start=1):
+        print(f'{idx}. Student:')
+        print('przedstaw_sie():', end=' ')
+        student.przedstaw_sie()
+        print(f'__str__(): {student}\n')
 
 if __name__ == '__main__':
     test_basic()
